bdc042624d715da9244a18c96d37debfb8a226c8
Stanisław Zagórowski <hasohasacz@gmail.com>
2023-07-02 17:06:19 +0200

    utils: add dump_buffer

---
+++ subprojects/utils/include/utils.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "types.h"
+
+#include <stdio.h>
+
+void dump_buffer(const u8* buffer, usize buffer_size, usize width);
---
+++ subprojects/utils/src/utils.c
@@ -0,0 +1,16 @@
+#include "utils.h"
+
+#include <stdio.h>
+
+void dump_buffer(const u8* buffer, usize buffer_size, usize width) {
+    printf("buffer_size: %ld\n", buffer_size);
+    printf("buffer: [\n");
+    printf("0000:");
+    for (usize i = 0; i < buffer_size; i++) {
+        printf("\t0x%02x,", buffer[i]);
+        if (i % width == width - 1) {
+            printf("\n%04lx:", i + 1);
+        }
+    }
+    printf("\n]\n");
+}
